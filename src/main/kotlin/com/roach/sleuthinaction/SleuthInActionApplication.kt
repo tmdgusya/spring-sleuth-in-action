4aa1eac1639affbeb8b82977c0a959d4c83909dd
tmdgusya <tmdgusya@naver.com>
2022-04-27 18:59:20 +0900

    init

---
+++ src/main/kotlin/com/roach/sleuthinaction/SleuthInActionApplication.kt
@@ -0,0 +1,11 @@
+package com.roach.sleuthinaction
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SleuthInActionApplication
+
+fun main(args: Array<String>) {
+	runApplication<SleuthInActionApplication>(*args)
+}
---
+++ src/test/kotlin/com/roach/sleuthinaction/SleuthInActionApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.roach.sleuthinaction
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SleuthInActionApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
